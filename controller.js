28bf54d8e2f34eb2a844427cf709dad54b2c431d
Steven Cassidy <steven.cassidy@gmail.com>
2015-11-15 12:55:15 -0500

    Remove some cruft

--- controller.js
+++ controller.js
@@ -8,13 +8,11 @@ App.Controller.prototype.renderView = function() {
 }
 
 App.Controller.prototype.updateTask = function(args) {
-  console.log(args);
   this.todoList.updateTask(args);
   this.renderView();
 }
 
 App.Controller.prototype.addTask = function(args) {
-  console.log(args);
   var t = new App.Task(args);
   this.todoList.addTask(t);
   this.renderView();
--- setup.js
+++ setup.js
@@ -5,10 +5,5 @@ document.addEventListener('DOMContentLoaded', function(event){
   app.controller = new App.Controller(app.todoList, app.view);
   app.view.controller = app.controller;
   app.controller.renderView();
-
-//  app.controller.addTask({taskName: 'Buy milk', done:false});
-//  app.controller.addTask({taskName: 'Buy dog food', done:true});
-//  app.controller.addTask({taskName: 'Get new job', done:false});
-
 });
 
